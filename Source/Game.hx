5d57861b5d1d1ac64bdca4b01e8017e68621291f
steffansk1997 <steffanvenema@hotmail.com>
2015-01-23 23:38:34 +0100

    Added init point and Character/Vehicle Variable

--- Source/Game.hx
+++ Source/Game.hx
@@ -1,15 +1,25 @@
 package ;
+import openfl.display.Sprite;
+import openfl.Assets;
+import openfl.events.Event;
 
 /**
  * ...
  * @author ...
  */
-class Game
+class Game extends Sprite
 {
+	var character:Character = new Character;
+	var vehicle:Vehicle = new Vehicle;
 
 	public function new() 
 	{
-		
+		super();
+		addEventListener(Event.ADDED_TO_STAGE, init)
 	}
 	
+	//Called when the Game Class is added to the stage by the main class
+	function init(e:Event) {
+		
+	}
 }
