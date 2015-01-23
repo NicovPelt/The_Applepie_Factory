fe51184c70a78a7d3dd8bbe874c89089dd7ea74a
SteelDragony <blaweetniet@live.nl>
2015-01-24 00:54:31 +0100

    Pre Alpha 0.03

--- Source/Game.hx
+++ Source/Game.hx
@@ -10,21 +10,34 @@ import openfl.events.Event;
  */
 class Game extends Sprite
 {
-	var character:Character = new Character;
-	var vehicle:Vehicle = new Vehicle;
-
+	var character:Character = new Character();
+	var vehicle:Vehicle;
+	var terrain:Terrain;
+	var inited:Bool = false;
+	
 	public function new() 
 	{
 		super();
-		addEventListener(Event.ADDED_TO_STAGE, init)
+		addEventListener(Event.ADDED_TO_STAGE, init);
 	}
 	
 	//Called when the Game Class is added to the stage by the main class
 	function init(e:Event) 
 	{
-		var terrain = new Terrain();
+		terrain = new Terrain();
 		addChild(terrain);
-		var vehicle = new Vehicle(Std.int(stage.stageWidth/2), Std.int(stage.stageHeight/2));
+		vehicle = new Vehicle(Std.int(stage.stageWidth/2), Std.int(stage.stageHeight/2), terrain);
 		addChild(vehicle);
+		inited = true;
+	}
+	
+	public function update()
+	{
+		if (inited)
+		{
+		terrain.update();	
+		vehicle.update();
+		}
+
 	}
 }
--- Source/vehicle/Vehicle.hx
+++ Source/vehicle/Vehicle.hx
@@ -8,19 +8,35 @@ import openfl.display.Sprite;
  */
 class Vehicle extends Sprite
 {
-
-	public function new(xPos:Int, yPos:Int) 
+	var terrain:Terrain;
+	var speed:Int = 4;
+	public function new(xPos:Int, yPos:Int, terrainRef:Terrain ) 
 	{
 		super();
 		draw();
 		this.x = xPos;
 		this.y = yPos;
+		terrain = terrainRef;
 	}
 	
 	function draw()
 	{
 		graphics.beginFill(0x00ff00);
-		graphics.drawRoundRect(0, 0, 200, 200, 10, 10);
+		graphics.drawRoundRect(-100, -100, 200, 200, 10, 10);
 		graphics.endFill();
+	}
+	
+	public function update()
+	{
+		moveLeft();
+	}
+	
+	function moveLeft()
+	{
+		terrain.x += speed;
+	}
+	function moveRight()
+	{
+		terrain.x -= speed;
 	}
 }
