bf897754b4cb6c0fa5130ef65c09a3831ee36b4b
steffansk1997 <steffanvenema@hotmail.com>
2015-01-24 00:40:35 +0100

    Added a draw for the character and character images

--- Source/Character.hx
+++ Source/Character.hx
@@ -1,6 +1,10 @@
 package ;
 
 import openfl.display.Sprite;
+import openfl.display.Tilesheet;
+import openfl.geom.Rectangle;
+import openfl.events.Event;
+import openfl.Assets;
 
 /**
  * ...
@@ -8,11 +12,39 @@ import openfl.display.Sprite;
  */
 class Character extends Sprite
 {
-
+	var character:Tilesheet = new Tilesheet(Assets.getBitmapData("img/character/doofus.png"));
+	var tileHeight:Int = 32;
+	var tileWidth:Int = 32;
+	var tiles:Int = 1;
+	
+	
 	public function new() 
 	{
 		super();
+		addEventListener(Event.ADDED_TO_STAGE, init);
 		
 	}
 	
+	function init(e:Event) {
+		initTiles();
+		drawCharacter
+	}
+	
+	function drawCharacter():Void {
+		this.graphics.clear();
+		character.drawTiles( this.graphics, [ 0, 0, 0], true );
+		this.x = (stage.stageWidth - this.width) / 2;
+		this.y = (stage.stageHeight - this.height) /2;
+		
+	}
+	function initTiles():Void {
+		var column:Int = 0;
+		var row:Int = 0;
+		for( i in 0...tiles ){
+			var charRect:Rectangle = new Rectangle( tileWidth * column, tileHeight * row, tileWidth, tileHeight );
+			character.addTileRect( gooseRectangle );
+			row = Math.floor( i / 3 );
+			column = i % 3;
+		}
+	}
 }
