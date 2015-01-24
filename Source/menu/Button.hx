6f5c23711bbab2a680cddfbea5e05285aac6400c
steffansk1997 <steffanvenema@hotmail.com>
2015-01-25 00:20:23 +0100

    Entry for menu

---
+++ Source/menu/Button.hx
@@ -0,0 +1,18 @@
+package menu;
+
+import openfl.display.Sprite;
+
+/**
+ * ...
+ * @author steffansk1997
+ */
+class Button extends Sprite
+{
+
+	public function new() 
+	{
+		super();
+		
+	}
+	
+}
---
+++ Source/menu/Menu.hx
@@ -0,0 +1,17 @@
+package menu ;
+import openfl.display.Sprite;
+/**
+ * ...
+ * @author steffansk1997
+ */
+class Menu extends Sprite
+{
+	var start:Bool = false;
+	
+	public function new() 
+	{
+		super();
+		
+	}
+	
+}
