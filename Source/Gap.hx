c86aad5c03e0973140bfd43efbf8a0624b85e258
steffansk1997 <steffanvenema@hotmail.com>
2015-01-25 12:52:33 +0100

    Made the gap more sensitive

--- Source/Gap.hx
+++ Source/Gap.hx
@@ -23,14 +23,15 @@ class Gap extends Sprite
 	function drawGap() {
 
 		this.graphics.beginFill(0xff0000);
-		this.graphics.drawRect( 0, 0, 140, 100);
+		this.graphics.drawRect( 0, 0, 140, 150);
 		this.graphics.endFill();
 		this.x = 2360;
 		this.y = stage.stageHeight - this.height /2;
-		this.alpha =0;
+		this.alpha =1;
 	}
 	public function update() {
 		if (this.hitTestObject(rock)) {
+			
 			if (!rock.grabbed) rock.x = this.x + this.width / 2;
 			isFilled = true;
 			rock.inGap = true;
