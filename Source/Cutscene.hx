5bbae5fec3f4c863e336baacb1d9d5771538015b
steffansk1997 <steffanvenema@hotmail.com>
2015-01-25 09:04:27 +0100

    Cutscene now adapts to screen H & W

--- Source/Cutscene.hx
+++ Source/Cutscene.hx
@@ -4,6 +4,7 @@ import openfl.display.Sprite;
 import openfl.Assets;
 import haxe.Timer;
 import openfl.display.Bitmap;
+import openfl.events.Event;
 
 /**
  * ...
@@ -19,12 +20,18 @@ class Cutscene extends Sprite
 	{
 		super();
 		this.main = main;
+		addEventListener(Event.ADDED_TO_STAGE, init);
+	}
+	function init(e:Event) {
 		nextFrame();
 	}
 	
 	function nextFrame() {
 		removeChildren();
-		addChild(new Bitmap(Assets.getBitmapData("img/cutscene/cutscene" + frameNr + ".png")));
+		var frame:Bitmap = new Bitmap(Assets.getBitmapData("img/cutscene/cutscene" + frameNr + ".png"));
+		frame.width = stage.stageWidth;
+		frame.height = stage.stageHeight;
+		addChild(frame);
 		frameNr++;
 		if(frameNr<21){
 			Timer.delay(nextFrame, 500);
