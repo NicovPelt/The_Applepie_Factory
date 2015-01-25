ba9971f03b458fec8b9d958e75813a54d9945acf
Nico <nicobn@live.nl>
2015-01-25 14:07:06 +0100

    ADDED 2ND OBSTACLE

--- Source/Pillar.hx
+++ Source/Pillar.hx
@@ -4,6 +4,7 @@ import openfl.display.Bitmap;
 import openfl.display.Sprite;
 import openfl.Assets;
 import openfl.events.Event;
+import haxe.Timer;
 
 /**
  * ...
@@ -14,6 +15,7 @@ class Pillar extends Sprite
 	var rock:GrabbableObject;
 	var pillarGraphics:Bitmap = new Bitmap(Assets.getBitmapData("assets/img/Background/Wall/Fixed1.png"));
 	public var destroyed:Bool = false;
+	var frame:Int = 1;
 
 	public function new(rock:GrabbableObject) 
 	{
@@ -36,8 +38,19 @@ class Pillar extends Sprite
 	}
 	
 	public function update() {
-		if (this.hitTestObject(rock)) {
-			
+		if (this.hitTestObject(rock) && !destroyed) {
+			destroyed = true;
+			crumble();
+		}
+	}
+	
+	function crumble() {
+		if (frame < 6) {
+			removeChild(pillarGraphics);
+			pillarGraphics = new Bitmap(Assets.getBitmapData("assets/img/Background/Wall/Fixed" + frame+".png"));
+			addChild(pillarGraphics);
+			frame++;
+			Timer.delay(crumble, 300);
 		}
 	}
 	
